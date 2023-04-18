f3b6b7bce5d151b208e104d7a173b95ab91c847a
Daniele Simoncini <simoncini@codeninja.it>
2023-04-18 12:57:15 +0200

    Aggiungere i file di progetto.

---
+++ primaApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+int chiedi(string domanda){
+    // chiedo all'utente che tabellina vuole
+    Console.Write(domanda);
+    var risposta = Console.ReadLine();
+    var numero = int.Parse(risposta);
+    return numero;
+}
+
+int moltiplicazione(int a, int b)
+{
+    // calcolo la moltpilicazione
+    int risultato = a * b;
+    // restituisco il risultato
+    return risultato;
+}
+
+
+
+var numero = chiedi("Tabellina : ");
+var ripetizioni = chiedi("Ripetizioni : ");
+
+Console.WriteLine("controllo in testa");
+// sfrutto una variabile per ricordarmi 1
+var volta = 0;
+// ripeto per 100 volte 
+while (volta < ripetizioni)
+{
+    Console.Write(numero);
+    Console.Write(" x ");
+    Console.Write(volta);
+    Console.Write(" = ");
+    // il risultato della tabellina
+    Console.WriteLine(   moltiplicazione(numero, volta)  );
+    // ricordandomi di cambiare lo stato
+    volta = volta + 1;
+}
