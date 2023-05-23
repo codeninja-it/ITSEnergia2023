c9e4b6546ade32f4ed032aabe9d7b57ddb4a4d7a
Daniele Simoncini <simoncini@codeninja.it>
2023-05-23 13:00:19 +0200

    esercizio di produzione di due parole collegate insieme, la prima intera, la seconda dove vengono mantenute le sole consonanti

--- SettimaApp/Form1.Designer.cs
+++ SettimaApp/Form1.Designer.cs
@@ -1,87 +1,102 @@
 ﻿namespace SettimaApp
 {
-	partial class Form1
-	{
-		/// <summary>
-		///  Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		///  Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			txtTesto = new TextBox();
-			btnEstrai = new Button();
-			lstRisultati = new ListBox();
-			SuspendLayout();
-			// 
-			// txtTesto
-			// 
-			txtTesto.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-			txtTesto.Location = new Point(12, 12);
-			txtTesto.Multiline = true;
-			txtTesto.Name = "txtTesto";
-			txtTesto.ScrollBars = ScrollBars.Vertical;
-			txtTesto.Size = new Size(809, 707);
-			txtTesto.TabIndex = 0;
-			// 
-			// btnEstrai
-			// 
-			btnEstrai.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-			btnEstrai.Location = new Point(12, 725);
-			btnEstrai.Name = "btnEstrai";
-			btnEstrai.Size = new Size(809, 46);
-			btnEstrai.TabIndex = 1;
-			btnEstrai.Text = "estrai";
-			btnEstrai.UseVisualStyleBackColor = true;
-			btnEstrai.Click += btnEstrai_Click;
-			// 
-			// lstRisultati
-			// 
-			lstRisultati.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
-			lstRisultati.FormattingEnabled = true;
-			lstRisultati.ItemHeight = 32;
-			lstRisultati.Location = new Point(837, 12);
-			lstRisultati.Name = "lstRisultati";
-			lstRisultati.Size = new Size(519, 740);
-			lstRisultati.TabIndex = 2;
-			// 
-			// Form1
-			// 
-			AutoScaleDimensions = new SizeF(13F, 32F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(1368, 788);
-			Controls.Add(lstRisultati);
-			Controls.Add(btnEstrai);
-			Controls.Add(txtTesto);
-			Name = "Form1";
-			Text = "Form1";
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTesto = new TextBox();
+            btnEstrai = new Button();
+            lstRisultati = new ListBox();
+            btnEsercizio = new Button();
+            SuspendLayout();
+            // 
+            // txtTesto
+            // 
+            txtTesto.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtTesto.Location = new Point(12, 12);
+            txtTesto.Multiline = true;
+            txtTesto.Name = "txtTesto";
+            txtTesto.ScrollBars = ScrollBars.Vertical;
+            txtTesto.Size = new Size(868, 608);
+            txtTesto.TabIndex = 0;
+            // 
+            // btnEstrai
+            // 
+            btnEstrai.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnEstrai.Location = new Point(176, 626);
+            btnEstrai.Name = "btnEstrai";
+            btnEstrai.Size = new Size(704, 46);
+            btnEstrai.TabIndex = 1;
+            btnEstrai.Text = "estrai";
+            btnEstrai.UseVisualStyleBackColor = true;
+            btnEstrai.Click += btnEstrai_Click;
+            // 
+            // lstRisultati
+            // 
+            lstRisultati.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            lstRisultati.FormattingEnabled = true;
+            lstRisultati.IntegralHeight = false;
+            lstRisultati.ItemHeight = 32;
+            lstRisultati.Location = new Point(896, 12);
+            lstRisultati.Name = "lstRisultati";
+            lstRisultati.Size = new Size(519, 660);
+            lstRisultati.TabIndex = 2;
+            // 
+            // btnEsercizio
+            // 
+            btnEsercizio.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnEsercizio.Location = new Point(12, 626);
+            btnEsercizio.Name = "btnEsercizio";
+            btnEsercizio.Size = new Size(150, 46);
+            btnEsercizio.TabIndex = 3;
+            btnEsercizio.Text = "esercizio";
+            btnEsercizio.UseVisualStyleBackColor = true;
+            btnEsercizio.Click += btnEsercizio_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1427, 689);
+            Controls.Add(btnEsercizio);
+            Controls.Add(lstRisultati);
+            Controls.Add(btnEstrai);
+            Controls.Add(txtTesto);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private TextBox txtTesto;
-		private Button btnEstrai;
-		private ListBox lstRisultati;
-	}
+        private TextBox txtTesto;
+        private Button btnEstrai;
+        private ListBox lstRisultati;
+        private Button btnEsercizio;
+    }
 }
